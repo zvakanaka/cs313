649b6a1c8249754bbec7f829a1c757ae4de0c93f
Adam Quinton <quintonish@gmail.com>
2015-01-21 20:14:52 -0700

    removed lame wording from footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <nav>
     <ul>
-    <li>Contact: Adam Quinton <a href="mailto:qui10001@byui.edu">Email</a> </li>
+    <li>Adam Quinton <a href="mailto:qui10001@byui.edu">Email</a> </li>
       <li><a href="http://www.github.com/zvakanaka">Github</a> </li>  
     </ul>
   </nav>
