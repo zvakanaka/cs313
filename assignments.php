ad66500b7738988204771ed72c49ff9b49838cc6
Adam Quinton <quintonish@gmail.com>
2015-01-24 21:54:58 -0700

    Changed name of demo to validate

--- assignments.php
+++ assignments.php
@@ -14,7 +14,7 @@ include 'header.php'
     <div>
     <article>
       <h1>Assignments</h1><br><br>
-      <a href="demo.php">PHP Survey</a>
+      <a href="surveyValidate.php">PHP Survey</a>
     </article>
     <?php include 'footer.php' ?>
     </div>
