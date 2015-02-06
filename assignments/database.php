abd1132e7c9aa3e13f41046fb8d59ab43244040e
Adam Quinton <quintonish@gmail.com>
2015-02-06 09:05:58 -0700

    fixed debug line and style

--- assignments/database.php
+++ assignments/database.php
@@ -3,43 +3,58 @@
   <head>
     <title>Aquinton | Database</title>
     <link rel="favorite icon" type="image/x-icon" href="favicon.ico" />
-        <link href="css/tanga.css" type="text/css" rel="stylesheet" media="screen">
+        <link href="../css/tanga.css" type="text/css" rel="stylesheet" media="screen">
   </head>
   <body>
 <?php
-include 'header.php'; 
+include '../header.php'; 
 ?>
     <div>
     <article>
 <?php
-try
-{
-   $user = "php";
-   $password = "php-pass";
-   $db = new PDO("mysql:host=127.0.0.1;dbname=pluckit", $user, $password);
-}
-catch (PDOException $ex)
-{
-   echo "Error!: " . $ex->getMessage();
-   die();
-}
+  $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+  if ($openShiftVar === null || $openShiftVar == "")
+  {
+    // Not in the openshift environment
+    $dbHost = "localhost";
+    try
+    {
+      $user = "php";
+      $password = "php-pass";
+      $db = new PDO("mysql:host=127.0.0.1;dbname=pluckit", $user, $password);
+    }
+    catch (PDOException $ex)
+    {
+      echo "Error!: " . $ex->getMessage();
+      die();
+    }
+  }
+  else
+  {
+    // In the openshift environment
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $dbName = "pluckit";
 
-foreach ($db->query("SELECT song_name, song_composer FROM song") as $row)
-{
-   echo "<h1>Songs</h1> " . $row['song_name'];
-   echo "<h1>Composer</h1> " . $row['song_composer'];
-   echo "<br />";
-echo "<h1>Chords </h1>";
-foreach ($db->query("SELECT chord_name FROM chord") as $row)
-{
-   echo $row['chord_name'];
-   echo " ";
-}
-
-}
+    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  } 
+  foreach ($db->query("SELECT song_name, song_composer FROM song") as $row)
+  {
+    echo "<h1>Songs</h1> " . $row['song_name'];
+    echo "<h1>Composer</h1> " . $row['song_composer'];
+    echo "<br />";
+    echo "<h1>Chords </h1>";
+    foreach ($db->query("SELECT chord_name FROM chord") as $row)
+    {
+      echo $row['chord_name'];
+      echo " ";
+    }
+  }
 ?>
 </article>
-<?php include 'footer.php' ?>
+<?php include '../footer.php' ?>
 </div>
 </body>
 </html>
--- assignments/query.php
+++ assignments/query.php
@@ -1,43 +1,55 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Aquinton | Database</title>
-    <link rel="favorite icon" type="image/x-icon" href="favicon.ico" />
-        <link href="../css/tanga.css" type="text/css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-<?php
-include '../header.php';
-?>
-<div>
-<article>
-<?php
-$dbName = "pluckit";
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+<head>
+	<title>Aquinton | Database</title>
+	<link rel="favorite icon" type="image/x-icon" href="favicon.ico" />
+	<link href="../css/tanga.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+<body>
+	<?php
+	include '../header.php';
+	?>
+	<div>
+		<article>
+			<?php
+			$dbName = "pluckit";
 
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+			if ($openShiftVar === null || $openShiftVar == "") {
+				try
+				{
+					$user = "php";
+					$password = "php-pass";
+					$db = new PDO("mysql:host=127.0.0.1;dbname=pluckit", $user, $password);
+				}
+				catch (PDOException $ex)
+				{
+					echo "Error!: " . $ex->getMessage();
+					die();
+				}
+			} else {
+				$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+				$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+				$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+				$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+				echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
+				$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+			}
+			foreach ($db->query("SELECT song_name, song_composer FROM song") as $row)
+			{
+				echo "<h1>Songs</h1> " . $row['song_name'];
+				echo "<h1>Composer</h1> " . $row['song_composer'];
+				echo "<br />";
+				echo "<h1>Chords </h1>";
 
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-/*
-foreach ($db->query("SELECT song_name, song_composer FROM song") as $row)
-{
-   echo "<h1>Songs</h1> " . $row['song_name'];
-   echo "<h1>Composer</h1> " . $row['song_composer'];
-   echo "<br />";
-echo "<h1>Chords </h1>";
-
-foreach ($db->query("SELECT chord_name FROM chord") as $row)
-{
-   echo $row['chord_name'];
-   echo " ";
-}
-*/
-?>
-</article>
-</div>
+				foreach ($db->query("SELECT chord_name FROM chord") as $row)
+				{
+					echo $row['chord_name'];
+					echo " ";
+				}
+			}
+			?>
+		</article>
+	</div>
 </body>
 </html>
