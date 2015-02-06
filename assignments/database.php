44c3f0707d3a259537f3701689b9d7351c7c0152
Adam Quinton <quintonish@gmail.com>
2015-02-05 23:16:35 -0700

    Update database.php

--- assignments/database.php
+++ assignments/database.php
@@ -12,12 +12,7 @@ include '../header.php';
     <div>
     <article>
 <?php
-//$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-
 if ($openShiftVar === null || $openShiftVar == "")
 {
      // Not in the openshift environment
@@ -42,6 +37,7 @@ else
      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     $dbName = "pluckit";
      echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
      $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
