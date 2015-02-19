a55d0ba22efd27514c478942ccc4c424af731bfa
Adam Quinton <quintonish@gmail.com>
2015-02-18 22:34:53 -0700

    Fix missing OpenShift DB Script

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <nav style="text-align: right">
     <ul>
-    <li>Contact Adam Quinton <a href="mailto:qui10001@byui.edu" title="Send me an email">Email</a> </li>
+    <li>  <?php echo 'Last updated: <time datetime="'. date('c') . '">' . date('F j,Y, g:i a', getlastmod()) . '</time>'; ?>      Contact Adam Quinton <a href="mailto:qui10001@byui.edu" title="Send me an email">Email</a> </li>
       <li><a href="http://www.github.com/zvakanaka" title="My Github Repositories">Github</a> </li>  
     </ul>
   </nav>
